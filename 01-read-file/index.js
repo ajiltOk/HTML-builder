2ffadb1ff9f86cca75b40ccb9563db2ef9216f2b
ajiltOk <21summer12@gmail.com>
2023-05-04 16:04:29 +0300

    feat: add solution of task (01-read-file)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.readFile(
-    path.join(__dirname, './01-read-file', '../text.txt'),
-    'utf-8',
-    (err, data) => {
-        if (err) throw err;
-        console.log(data);
-    }
-);
+let stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), {encoding: 'utf-8'});
+
+stream.on('readable', function(){
+    let data = stream.read();
+    console.log(data);
+});
